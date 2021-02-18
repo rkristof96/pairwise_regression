a214db16cceb4a2fae8d97079c88606f98711627
rgyuri <ruzicska.gyorgy@gmail.com>
2021-02-18 16:11:59 +0100

    Added Second_cost_function

---
+++ MATLAB_codes/Second_cost_function/costfunction2_beta1.m
@@ -0,0 +1,12 @@
+function retval = costfunction2_beta1(beta)
+
+%using global variables w and x;
+global delta_x;
+global pairwise_beta1;
+
+weighted_product = pairwise_beta1 .* delta_x';
+beta_diff = weighted_product - beta;
+beta_diff_squared = beta_diff.^2;
+sum_of_beta_diff_squared=sum(beta_diff_squared);
+
+retval = sum_of_beta_diff_squared; %define the return value of the function
